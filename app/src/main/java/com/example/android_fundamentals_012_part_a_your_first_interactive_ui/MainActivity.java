81ad0f0a7c0cb98d8a1cb15db67788cd38ff9d08
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-03-27 12:33:27 +0100

    [AF012] Create the Project

---
+++ app/src/main/java/com/example/android_fundamentals_012_part_a_your_first_interactive_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_fundamentals_012_part_a_your_first_interactive_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
