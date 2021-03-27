2d79915e942ae670c4fa78c2c44facbc40107d1a
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-03-27 13:19:29 +0100

    [AF0102] Finish challenge  and extra
    Created an app with one activity MainActivity. With two buttons and a TextView.
    
    One Button changes the value stored in the text view and the other shows a Toast message that shows the user the current value stored in the text view.
    
    The buttons shares  a one onClick method the MainActOnClick. On the basis of the different button id I call the right function for the right button click. using the switch case command.
    
    I added a landscape layout for the main activity and saved the value in the ovverriden onSaveInstanceState() method in the bundle wiht a created constant string as key,  to prevent loesing the value by rotating the screen of the application.

--- app/src/main/java/com/example/android_fundamentals_012_part_a_your_first_interactive_ui/MainActivity.java
+++ app/src/main/java/com/example/android_fundamentals_012_part_a_your_first_interactive_ui/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.android_fundamentals_012_part_a_your_first_interactive_ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String EXTRA_VALUE = "MainActivity.extras.integer_value";
+    TextView tvValue;
+    Integer value = 0;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(EXTRA_VALUE, value);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvValue = findViewById(R.id.tV_value);
+
+        if (savedInstanceState != null)
+        {
+            value = savedInstanceState.getInt(EXTRA_VALUE);
+            tvValue.setText(value.toString());
+        }
     }
+
+    public void MainActOnClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.btn_Toast:
+                showToast();
+                break;
+
+            case R.id.btn_count:
+                count();
+                break;
+        }
+
+    }
+
+    public void showToast()
+    {
+        Toast.makeText(this, "The current value is: " + tvValue.getText(), Toast.LENGTH_LONG).show();
+    };
+
+    public void count()
+    {
+        ++value;
+        tvValue.setText(value.toString());
+    };
+
 }
